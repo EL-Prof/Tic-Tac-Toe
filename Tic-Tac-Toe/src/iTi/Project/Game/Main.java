6498b8b5b63518e8d3ba87afc3a41ffa492d628e
AndrewS <AndrewS@Andrew>
2016-12-30 18:32:20 +0200

    test

--- Tic-Tac-Toe/src/iTi/Project/Game/Main.java
+++ Tic-Tac-Toe/src/iTi/Project/Game/Main.java
@@ -10,5 +10,30 @@ package iTi.Project.Game;
  * @author Ahmed Emad el-deen
  */
 public class Main {
+    public static void main(String[] args) {
+        
+    char XO[][] = new char[3][3];
+   
+    for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++) 
+    XO[i][j] = '-';
+    
+    int turn=1;    
+    int index;
+    
+    
+    }
+      
+    
+   /*
+    functions decleration in UI:
+    void selectCell(int cell,int player);
+    int setCell(int player);  if return 1 so the game is finished
+    void getcell(int player); parameter is the player number
+    
+    functions decleration in joystic:
+    void intialize()
+    */
+    
     
 }
