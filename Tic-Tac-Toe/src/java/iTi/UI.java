240307891d094da3c3bb31c49d1e72453f9a6cf8
Ahmed Emad el-deen <Ahmed Emad el-deen@192.168.1.11>
2016-12-30 17:12:38 +0200

    Project initialization

---
+++ Tic-Tac-Toe/src/java/iTi/UI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.iTi;
+
+/**
+ *
+ * @author Ahmed Emad el-deen
+ */
+public class UI {
+    
+}
