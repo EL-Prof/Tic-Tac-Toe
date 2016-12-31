5ca7b8c076424175bea90df36775d676074c9ae5
AndrewS <AndrewS@Andrew>
2016-12-31 09:44:07 +0200

    test

--- Tic-Tac-Toe/src/iTi/Project/Game/Main.java
+++ Tic-Tac-Toe/src/iTi/Project/Game/Main.java
@@ -14,26 +14,57 @@ public class Main {
         
     char XO[][] = new char[3][3];
    
+    int player=0;
+    int curr;
     for (int i = 0; i < 3; i++)
     for (int j = 0; j < 3; j++) 
     XO[i][j] = '-';
     
-    int turn=1;    
-    int index;
+    while( !Joystic.initialize() ) {   }
     
+    while(true)
+    {
+        if( UI.isFinished() )
+        {
+            break;
+        }
+        curr=UI.getCell(player+1);
+        switch (Joystic.getChar()) {
+            case 'X':
+            case 'O':
+                UI.setCell(player+1);
+                player=~player;
+                break;
+            case 'U':                
+                if( curr!=1 || curr!=2 ||curr!=3)
+                {
+                    UI.selectCell( curr-3 ,player+1);
+                }
+                break;
+            case 'D':
+                if( curr!=7 || curr!=8 ||curr!=9)
+                {
+                    UI.selectCell( curr+3 ,player+1);
+                }
+                break;
+            case 'R':
+                if( curr!=3 || curr!=6 ||curr!=9)
+                {
+                    UI.selectCell( curr+1 ,player+1);
+                }
+                break;
+            case 'L':
+                if( curr!=1 || curr!=4 ||curr!=7)
+                {
+                    UI.selectCell( curr-3 ,player+1);
+                }
+                break;
+            default:
+                UI.selectCell( 0 ,player+1);
+                break;
+        }
+    }    
     
     }
-      
-    
-   /*
-    functions decleration in UI:
-    void selectCell(int cell,int player);
-    int setCell(int player);  if return 1 so the game is finished
-    void getcell(int player); parameter is the player number
-    
-    functions decleration in joystic:
-    void intialize()
-    */
-    
     
 }
